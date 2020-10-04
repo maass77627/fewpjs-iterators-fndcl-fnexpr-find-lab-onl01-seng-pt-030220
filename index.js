d8a8cd1e2eaac8431d7dc08139a7e1f0625bae8f
kelly r maass <kmaass77627@gmail.com>
2020-10-04 04:48:02 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+//   //...
+// ]
+
+
+function superbowlWin(record) {
+
+  const winner = record.find((record) => record.result === "W")
+    return !!winner ? winner.year : undefined
+
+
+}
