9015291371b7c6ebb0a3601cce7cf7ae258c1396
kelly r maass <kmaass77627@gmail.com>
2020-10-04 04:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+year and result
+
+function superbowlWin(Array) {
+
+  Array.find(function(object) {if (object.result === w) {return object.year})
+  
+}
